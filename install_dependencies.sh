e6bfc3ab2d9cdaff858990a0f3245ef72ab37532
itxxahsan123 <jacklewis0084@gmail.com>
2024-10-07 08:27:10 +0500

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-cd /home/ubuntu/my-pythonapp
-pip install -r requirements.txt
+# install_dependencies.sh
+
+echo "Installing system packages..."
+sudo apt update
+sudo apt install -y python3-venv python3-pip  # Add any other required packages here
+
+echo "Setting up the virtual environment..."
+python3 -m venv /home/ubuntu/my-pythonapp/venv
+source /home/ubuntu/my-pythonapp/venv/bin/activate
+
+echo "Installing Python dependencies in the virtual environment..."
+pip install -r /home/ubuntu/my-pythonapp/requirements.txt
+
+echo "Installation completed."
