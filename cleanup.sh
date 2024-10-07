2dfcbe12c2a80525d1e81507d4354be589c35fc3
itxxahsan123 <jacklewis0084@gmail.com>
2024-10-07 08:18:31 +0500

    Delete cleanup.sh

--- cleanup.sh
+++
@@ -1,5 +0,0 @@
-# cleanup.sh
-# Remove any existing files before copying new ones
-rm -f /home/ubuntu/my-pythonapp/install_dependencies.sh
-rm -f /home/ubuntu/my-pythonapp/app.py
-rm -f /home/ubuntu/my-pythonapp/cleanup.sh
